7c967883e71e1fb30b777e76a7549ba4c6301473
kalai1217 <162484182+kalai1217@users.noreply.github.com>
2024-04-22 16:21:51 +0530

    Updated source_to_bronze file

---
+++ src/assignment_1/source_to_bronze/employee_source_to_bronze.py
@@ -0,0 +1,37 @@
+# Databricks notebook source
+# MAGIC %run ./utils
+
+# COMMAND ----------
+
+# read all csv
+
+employee_path = "dbfs:/FileStore/assignments/assignment_1/resources/Employee_Q1.csv"
+department_path = "dbfs:/FileStore/assignments/assignment_1/resources/Department_Q1.csv"
+country_path= "dbfs:/FileStore/assignments/assignment_1/resources/Country_Q1.csv"
+
+country_df=read_csv_data(country_path)
+department_df=read_csv_data(department_path)
+employee_df=read_csv_data(employee_path)
+
+
+# COMMAND ----------
+
+# write all csv files
+
+write_csv_file(country_df,'dbfs:/FileStore/assignments/assignment_1/source_to_bronze/country_df.csv')
+write_csv_file(department_df,'dbfs:/FileStore/assignments/assignment_1/source_to_bronze/department_df.csv')
+write_csv_file(employee_df,'dbfs:/FileStore/assignments/assignment_1/source_to_bronze/employee_df.csv')
+
+# COMMAND ----------
+
+employee_df= change_column_case_to_snake_case(employee_df)
+department_df=change_column_case_to_snake_case(department_df)
+country_df=change_column_case_to_snake_case(country_df)
+
+# COMMAND ----------
+
+spark.sql("CREATE DATABASE IF NOT EXISTS Employee_info")
+
+# COMMAND ----------
+
+write_delta_table(employee_df,"Employee_info","dim_employee","EmployeeID","/silver/Employee_info/dim_employee")
